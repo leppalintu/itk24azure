ddb3682acc4a64dca1b717baa7433d1a521d0716
leppalintu <63555681+leppalintu@users.noreply.github.com>
2025-03-14 14:52:34 +0200

    Update index.php
    Lisasin tervituse ja veidi sisu

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
   <head>
     <title>ITK24 Azure</title>
   </head>
+  <body>
+    <h1> Meie projekt ITK24 gruppiga</h1>
+    <p>See leht arvestab meie muutustega (loe: ilgelt dünaamiline)</p>
+  </body>
 </html>
