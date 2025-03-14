18e14e601e6e4a87241cdab4f72f9ce159799bda
leppalintu <63555681+leppalintu@users.noreply.github.com>
2025-03-14 14:58:02 +0200

    Update index.php
    Lisasin parema pildi :)

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
   <body>
     <h1> Meie projekt ITK24 gruppiga</h1>
     <p>See leht arvestab meie muutustega (loe: ilgelt dünaamiline)</p>
-    <img src="https://lh3.googleusercontent.com/gps-cs-s/AB5caB9vNiAtjXCyxvuhRpz0thgqwmyup8aOlIMo4eogcU-zQoWY8nq1ruBwKpEk3PJ70Zs8IisS8Q6A0B_NZDf4HD8gqlu8oSpilSqJMTlh_NirShloUSmHNHHEZLUfFiVANO_LJGs=s1360-w1360-h1020">
+    <img src="https://m.media-amazon.com/images/I/61Mz2o910YL._AC_SL1500_.jpg">
   </body>
 </html>
